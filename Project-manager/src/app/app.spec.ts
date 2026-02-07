bf1a26703a8f06accc6c5ad0d0df0cb96a3c0d4a
AyoubGhoula <ayoubghoula40@gmail.com>
2026-02-07 10:58:10 +0100

    new project Angular

--- Project-manager/src/app/app.spec.ts
+++ Project-manager/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, n');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Project-manager');
   });
 });
